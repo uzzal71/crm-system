93cc3c083de9b15730aa55ebe991411ac8a386cf
uzzal71 <uzzalroy.acm@gmail.com>
2025-01-04 20:02:04 +0600

    react top loading bar add

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,28 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
+import { useEffect, useRef } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import LoadingBar, { LoadingBarRef } from "react-top-loading-bar";
 import CreateCampaign from "./pages/CreateCampaign";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 
-function App() {
+const App = () => {
+  const ref = useRef<LoadingBarRef>(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    ref.current?.continuousStart();
+    const timeout = setTimeout(() => {
+      ref.current?.complete();
+    }, 500);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [location]);
+
   return (
     <>
+      <LoadingBar color="#f11946" ref={ref} shadow={true} />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/create-campaign" element={<CreateCampaign />} />
@@ -14,6 +30,6 @@ function App() {
       </Routes>
     </>
   );
-}
+};
 
 export default App;
