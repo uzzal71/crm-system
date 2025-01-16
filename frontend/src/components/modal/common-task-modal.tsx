92a46d0eb93ddf2ba177988c797004d2e88fa685
uzzal71 <uzzalroy.acm@gmail.com>
2025-01-16 21:34:41 +0600

    test

--- frontend/src/components/modal/common-task-modal.tsx
+++ frontend/src/components/modal/common-task-modal.tsx
@@ -2,6 +2,12 @@ const CommonTaskModal = () => {
   return (
     <div>
       <h2>common-task-modal</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet earum,
+        molestiae odit hic voluptatem ipsa maxime sunt corrupti distinctio,
+        pariatur officia cumque accusantium quaerat dignissimos modi tempore
+        commodi rem vel!
+      </p>
     </div>
   );
 };
