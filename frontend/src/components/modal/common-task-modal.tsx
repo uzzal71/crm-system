2ee1154dc19b7cae980ec90a428d19026248c3e4
uzzal71 <uzzalroy.acm@gmail.com>
2025-01-20 21:59:40 +0600

    test

--- frontend/src/components/modal/common-task-modal.tsx
+++ frontend/src/components/modal/common-task-modal.tsx
@@ -15,6 +15,7 @@ const CommonTaskModal = () => {
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam aliquid, ut obcaecati maxime laudantium temporibus doloribus eum placeat reiciendis, blanditiis fuga corrupti nam nostrum adipisci autem numquam esse voluptatibus dignissimos.
       </p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis repellat consectetur, tenetur optio temporibus rerum totam quos ut iure. Fugiat dignissimos possimus necessitatibus totam quae tempore molestiae quaerat voluptate est!</p>
     </div>
   );
 };
